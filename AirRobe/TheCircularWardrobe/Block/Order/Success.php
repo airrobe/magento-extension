c0e04e46b21458b99e28b8d29c92ffa250652851
Dhaval Gajjar <dprogrammingjoomla2011@gmail.com>
2021-01-27 23:08:06 +0530

    Thank you page place holder

---
+++ AirRobe/TheCircularWardrobe/Block/Order/Success.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     AirRobe_TheCircularWardrobe
+ * @author      Michael Dawson <developers@airrobe.com>
+ * @copyright   Copyright AirRobe (https://airrobe.com/)
+ * @license     https://airrobe.com/license-agreement.txt
+ */
+namespace AirRobe\TheCircularWardrobe\Block\Order;
+
+class Success  extends \Magento\Framework\View\Element\Template
+{
+    public function getAirThanks()
+    {
+        return 'Hello AirRobe';
+    }
+}
