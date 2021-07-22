0db3109a0e7e6939e600d8392e7e40863433926e
Michael Dawson <m.dawson@y7mail.com>
2021-07-22 16:21:34 +0200

    Work on content security policy, configurable brand attribute, etc

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * @package   AirRobe_TheCircularWardrobe
- * @author    Michael Dawson <developers@airrobe.com>
- * @copyright Copyright AirRobe (https://airrobe.com/)
- * @license   https://airrobe.com/license-agreement.txt
- */
-
 namespace AirRobe\TheCircularWardrobe\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
